3ee381ea2356fd9d2d05ac637ca827003c501295
leb <leu.barycheuski@smals.be>
2017-12-13 14:38:14 +0100

    RotateArray

--- src/main/java/com/interviewcake/algolearn/RoateArray.java
+++ src/main/java/com/interviewcake/algolearn/RoateArray.java
@@ -6,18 +6,49 @@ public class RoateArray {
         if (k > nums.length) {
             k = k % nums.length;
         }
-            int[] result = new int[nums.length];
+        int[] result = new int[nums.length];
 
-            for (int i = 0; i < k; i++) {
-                result[i] = nums[nums.length-k+i];
-            }
+        for (int i = 0; i < k; i++) {
+            result[i] = nums[nums.length - k + i];
+        }
 
-            int j = 0;
-            for (int i = k; i < result.length; i++) {
-                result[i] = nums[j];
-                j++;
-            }
-            System.arraycopy(result,0,nums,0,nums.length);
+        int j = 0;
+        for (int i = k; i < result.length; i++) {
+            result[i] = nums[j];
+            j++;
+        }
+        System.arraycopy(result, 0, nums, 0, nums.length);
+    }
+
+    //O(1) space and in O(n)
+    public void rotateArray2(int[] arr, int order) {
+        if (arr == null || arr.length == 0 || order < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+
+        if (order > arr.length) {
+            order = order % arr.length;
+        }
+
+        //Length of first part
+        int a = arr.length - order;
+
+        reverse(arr, 0, a - 1);
+        reverse(arr,a,arr.length-1);
+        reverse(arr,0,arr.length-1);
+    }
+
+    private void reverse(int[] arr, int left, int right) {
+        if (arr == null || arr.length == 1) {
+            return;
+        }
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
     }
+}
 
